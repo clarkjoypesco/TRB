1ae9bd39cc7fd267063f04ada3da890b775a60c4
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-06-08 05:28:44 +0800

    Initial Commit

---
+++ myscoreboard/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>I was rendered from a component</p>
+    </div>
+  );
+}
+
+export default App;
